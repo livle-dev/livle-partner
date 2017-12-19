ef81be556159ef4cfa155f726f93c634fbf2321a
JWWon <wonjiwn@naver.com>
2017-12-19 21:15:00 +0900

    set text input box style

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -9,19 +9,8 @@ import session_bg from '../../images/background/session_bg.png';
 import { background } from '../../styles/javascript';
 import { strings, stringToCode } from '../../strings';
 
-// function stringToCode(data) {
-//   return data.split('\n').map(line => {
-//     return (
-//       <span key={line[0]}>
-//         {line}
-//         <br />
-//       </span>
-//     );
-//   });
-// }
-
 class Session extends Component {
-  state = { signUp: false };
+  state = { isLogin: true };
 
   componentWillMount() {
     if (this.props.authenticated) {
@@ -30,37 +19,45 @@ class Session extends Component {
   }
 
   render() {
+    const { isLogin } = this.state;
     return (
       <div
+        id="session"
         className="_fullscreen _flex _hcenter-position _vcenter-position"
         style={background(session_bg)}
       >
         <div className="session-container _row-direction">
-          <div className="typo-container">
-            <p className="_fs_48 _fw-semi-bold _white">
-              {stringToCode(strings.typoTitle)}
-            </p>
-            <p className="_fs_22 _fw-normal _white">
-              {stringToCode(strings.typoContent)}
-            </p>
+          <div className="typo-container _column-direction _hcenter-position">
+            <div className="typo-title">
+              <p className="_fs_48 _fw-semi-bold _white">
+                {stringToCode(strings.typoTitle)}
+              </p>
+            </div>
+            <div className="typo-text">
+              <p className="_fs_22 _fw-normal _lh-1 _white">
+                {stringToCode(strings.typoContent)}
+              </p>
+            </div>
           </div>
 
-          <div className="login-box-container _hcenter-position">
-            <div className="_row-direction">
+          <div className="login-box-container _column-direction _vcenter-position">
+            <div className="select-container _row-direction">
               <div
-                className="_flex_1 _hcenter-position _vcenter-position"
-                onClick={e => this.setState({ signUp: false })}
+                className={`_flex_1 _hcenter-position _vcenter-position ${!isLogin &&
+                  'unselected-container'}`}
+                onClick={e => this.setState({ isLogin: true })}
               >
-                <p>LOG IN</p>
+                <p className="_fs_36 _fw-bold _ls-4 _white">LOG IN</p>
               </div>
               <div
-                className="_flex_1 _hcenter-position _vcenter-position"
-                onClick={e => this.setState({ signUp: true })}
+                className={`_flex_1 _hcenter-position _vcenter-position ${isLogin &&
+                  'unselected-container'}`}
+                onClick={e => this.setState({ isLogin: false })}
               >
-                <p>SIGN UP</p>
+                <p className="_fs_36 _fw-bold _ls-4 _white">SIGN UP</p>
               </div>
             </div>
-            <div>{this.state.signUp ? <SignUp /> : <SignIn />}</div>
+            <div>{this.state.isLogin ? <SignIn /> : <SignUp />}</div>
           </div>
         </div>
       </div>
