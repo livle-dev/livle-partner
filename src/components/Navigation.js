4e4b45e7f27dee7dc1bea2189357f9d2841ef0c8
Jaeseong Seo <07js23@gmail.com>
2018-01-07 20:17:10 +0900

    Refactor and fix bugs of Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,93 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { signoutUser } from '../actions';
 // image
 import { logo } from '../images';
 
-const locations = {
-  users: 'users',
-  concerts: 'concerts',
-  add: 'add',
-};
+const Navigation = ({ user, signoutUser, location }) => {
+  const checkAuth = user.id;
+  const checkLocation = (path) => location.pathname === path
 
-class Navigation extends Component {
-  state = { location: locations.concerts };
-
-  checkLocation(location) {
-    return this.state.location === location;
-  }
-
-  render() {
-    const { user, signoutUser } = this.props;
-    const checkAuth = user.id;
-
-    return (
-      <nav
-        className="_navbar"
-        style={{ position: checkAuth ? 'relative' : 'absolute' }}>
-        {checkAuth ? (
-          <div className="_full-container _row-direction _vcenter-position">
-            <Link to="/concerts" className="_flex_1 _vcenter-position">
-              <img
-                src={
-                  user.isAdmin ? logo.logo_livle_admin : logo.logo_livle_partner
-                }
-                className="_logo"
-              />
-            </Link>
-            {user.isAdmin && (
-              <div className="_selecotr-container _row-direction">
-                <div
-                  className={`_selector _flex _vcenter-position _hcenter-position
-                  ${this.checkLocation(locations.users) && '_selector-border'}`}
-                  onClick={() => this.setState({ location: locations.users })}>
+  return (
+    <nav
+      className="_navbar"
+      style={{ position: checkAuth ? 'relative' : 'absolute' }}>
+      {checkAuth ? (
+        <div className="_full-container _row-direction _vcenter-position">
+          <Link to="/concerts" className="_flex_1 _vcenter-position">
+            <img
+              src={
+                user.isAdmin ? logo.logo_livle_admin : logo.logo_livle_partner
+              }
+              className="_logo"
+            />
+          </Link>
+          {user.isAdmin && (
+            <div className="_selecotr-container _row-direction">
+              <div
+                className={`_selector _flex _vcenter-position _hcenter-position
+                  ${checkLocation('/users') && '_selector-border'}`}>
                   <Link to="/users" className="_white _fs_20">
                     회원관리
                   </Link>
                 </div>
                 <div
                   className={`_selector _flex _vcenter-position _hcenter-position
-                  ${this.checkLocation(locations.concerts) &&
-                    '_selector-border'}`}
-                  onClick={() =>
-                    this.setState({ location: locations.concerts })
-                  }>
-                  <Link to="/concerts" className="_white _fs_20">
-                    공연관리
-                  </Link>
-                </div>
-                <div
-                  className={`_selector _flex _vcenter-position _hcenter-position
-                  ${this.checkLocation(locations.add) && '_selector-border'}`}
-                  onClick={() => this.setState({ location: locations.add })}>
+                  ${checkLocation('/concerts') && '_selector-border'}`}>
+                    <Link to="/concerts" className="_white _fs_20">
+                      공연관리
+                    </Link>
+                  </div>
+                  <div
+                    className={`_selector _flex _vcenter-position _hcenter-position
+                  ${checkLocation('/add') && '_selector-border'}`}>
                   <Link to="/add" className="_white _fs_20">
                     공연등록
                   </Link>
                 </div>
               </div>
-            )}
-            <p className="_flex _vcenter-position _white _fs_20">
-              {user.company}님
-            </p>
-            <Link
-              onClick={signoutUser}
-              className="_logout-button _flex _vcenter-position _hcenter-position"
-              to="/">
-              Logout
-            </Link>
-          </div>
-        ) : (
-          <div className="_full-container _row-direction _vcenter-position">
-            <img src={logo.logo_livle_partner} className="_logo" />
-          </div>
-        )}
-      </nav>
-    );
-  }
+          )}
+          <p className="_flex _vcenter-position _white _fs_20">
+            {user.company}님
+          </p>
+          <Link
+            onClick={signoutUser}
+            className="_logout-button _flex _vcenter-position _hcenter-position"
+            to="/">
+            Logout
+          </Link>
+        </div>
+      ) : (
+        <div className="_full-container _row-direction _vcenter-position">
+          <img src={logo.logo_livle_partner} className="_logo" />
+        </div>
+      )}
+    </nav>
+  );
 }
 
 function mapStateToProps(state) {
   return { user: state.auth };
 }
-export default connect(mapStateToProps, { signoutUser })(Navigation);
+export default withRouter(connect(mapStateToProps, { signoutUser })(Navigation));
