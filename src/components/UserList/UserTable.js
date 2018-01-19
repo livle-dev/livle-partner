ec25fc13cfd5b1013e8584b42b2b7dfdcbda979c
Jaeseong Seo <07js23@gmail.com>
2018-01-19 14:29:03 +0900

    Move functions and use ReservationCounter

--- src/components/UserList/UserTable.js
+++ src/components/UserList/UserTable.js
@@ -4,37 +4,44 @@ import { map, find, reduceRight } from 'lodash';
 import PropTypes from 'prop-types';
 // Views
 import Content from '../Content';
+import ReservationCounter from './ReservationCounter'
+
+const startOfSubscription = user => {
+  const formatter = dateString => {
+    const date = new Date(dateString);
+    return `${date.getMonth() + 1} / ${date.getDate()}`;
+  };
+  const start = reduceRight(user.subscriptions, (result, curr) => {
+    if (!result) return curr;
+    if (curr.next_subscription_id === result.id) return curr;
+    return result;
+  });
+  if (start) {
+    return formatter(start.from);
+  } else {
+    return '-';
+  }
+};
+
+const subscriptionStatus = user => {
+  const subs = user.subscriptions;
+  if (subs.length === 0) return '-'; // 구독한 적 없음
+  const unpaidSub = find(subs, s => !s.paid_at);
+  if (!unpaidSub) return 'X'; // 구독한 적 있으나 중단함
+  if (new Date(unpaidSub.from) < new Date()) {
+    return '미납';
+  } else {
+    return 'O'; // 정상 구독
+  }
+};
+
+const subscriptionLimit = user => {
+  const currSub = user.subscriptions[0]
+  if (!currSub) return '-'
+  return <ReservationCounter subscription={currSub} />
+}
 
 const UserTable = ({ users }) => {
-  const startOfSubscription = user => {
-    const formatter = dateString => {
-      const date = new Date(dateString);
-      return `${date.getMonth() + 1} / ${date.getDate()}`;
-    };
-    const start = reduceRight(user.subscriptions, (result, curr) => {
-      if (!result) return curr;
-      if (curr.next_subscription_id === result.id) return curr;
-      return result;
-    });
-    if (start) {
-      return formatter(start.from);
-    } else {
-      return '-';
-    }
-  };
-
-  const subscriptionStatus = user => {
-    const subs = user.subscriptions;
-    if (subs.length === 0) return '-'; // 구독한 적 없음
-    const unpaidSub = find(subs, s => !s.paid_at);
-    if (!unpaidSub) return 'X'; // 구독한 적 있으나 중단함
-    if (new Date(unpaidSub.from) < new Date()) {
-      return '미납';
-    } else {
-      return 'O'; // 정상 구독
-    }
-  };
-
   const userRows = map(users, u => (
     <div className="_table-row _body" key={u.id}>
       <div className="_flex_1">
@@ -43,6 +50,7 @@ const UserTable = ({ users }) => {
       </div>
       <div className="date _text-center">{startOfSubscription(u)}</div>
       <div className="short-text _text-center">{subscriptionStatus(u)}</div>
+      <div className="number _text-center">{subscriptionLimit(u)}</div>
       <div className="number _text-center">TODO</div>
     </div>
   ));
@@ -57,6 +65,7 @@ const UserTable = ({ users }) => {
           </div>
           <div className="date _text-center">구독 시작일자</div>
           <div className="short-text _text-center">구독상태</div>
+          <div className="number _text-center">현재 예약</div>
           <div className="number _text-center">월평균 예약 공연 수</div>
         </div>
         {userRows}
