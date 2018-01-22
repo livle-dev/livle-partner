0b5290ccf2237fe847ba2296dbb3e36ea6bb941b
Sanghyuk <saanghyuk@gmail.com>
2018-01-22 17:15:54 +0900

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 module.exports = env => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
